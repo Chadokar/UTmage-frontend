312794825c94de78d202c6478fbfb9d75cc587e0
Shubham Chadokar <shubhamchadokar36@gmail.com>
2024-12-26 03:12:38 +0530

    log the changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient: QueryClient = new QueryClient();
 
+console.log(process.env.REACT_APP_BACKEND_URL);
+console.log(process.env.REACT_APP_FILE_BACKEND_URL);
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
